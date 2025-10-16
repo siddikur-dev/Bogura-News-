bc461f69c9e55d85b96e33176bb9c30b6bdec842
siddikur-dev <siddikur.dev@gmail.com>
2025-10-16 20:05:34 +0600

    Implement Marquee & Design Navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavLink } from "react-router";
+import user from '../../src/assets/user.png'
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between mx-auto ">
+      <div className="text-gray-500 flex gap-4">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About </NavLink>
+        <NavLink to="career">Career</NavLink>
+      </div>
+      <div className="flex items-center gap-3">
+        <img src={user} alt="" />
+        <button className="btn btn-primary">Login</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,7 +1,9 @@
 import { format } from "date-fns";
 import React from "react";
+import Marquee from "react-fast-marquee";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { Link, Outlet } from "react-router";
+import Navbar from "../../Components/Navbar";
 
 const Home = () => {
   return (
@@ -13,6 +15,23 @@ const Home = () => {
           Journalism Without Fear or Favour
         </p>
         <p>{format(new Date(), "EEEE, MMMM dd, yyyy")}</p>
+        {/* Latest Section */}
+        <div className="w-11/12 mx-auto my-5 flex items-center gap-4 bg-base-300 p-3">
+          <p className="text-bold bg-primary px-3 py-2 font-bold text-white ">
+            Latest
+          </p>
+          <Marquee speed={20}>
+            <p className="text-bold text-gray-500 font-bold ">
+              Stay updated with the latest news, events, and stories from
+              Bogura. Your trusted source politics, sports, and community
+              updates.
+            </p>
+          </Marquee>
+        </div>
+        {/* Navbar */}
+        <nav className="w-11/12 mx-auto my-5">
+          <Navbar></Navbar>
+        </nav>
       </header>
       <main>
         {/* Nav Left*/}
