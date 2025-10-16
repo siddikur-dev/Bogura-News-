0113df2c841e6b011a7b966820b6bc80f6384d13
siddikur-dev <siddikur.dev@gmail.com>
2025-10-16 18:40:24 +0600

    Route Set-up, theme set up & header Design

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# React App
+# The Bogura News
 
-### Git Clone: https://github.com/siddikur-dev/react-router-setup.git
+<!-- ### Git Clone: https://github.com/siddikur-dev/react-router-setup.git -->
--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -4,10 +4,10 @@ import { Link, Outlet } from "react-router";
 
 const Home = () => {
   return (
-    <section>
+    <section className="container mx-auto">
       <header>
         {/* Title */}
-        <h2>The Bogura News</h2>
+        <h2 className="header-font font-bold text-6xl text-center my-5">The Bogura News</h2>
       </header>
       <main>
         {/* Nav Left*/}
